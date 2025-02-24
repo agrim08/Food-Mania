4ff03dbed07519c7c16c5fb03881a5676eb6430b
agrim08 <agrimgupta0805@gmail.com>
2025-02-24 15:40:52 +0530

    bug fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,6 @@ import Body from "./components/Body";
 import Error from "./components/Error";
 import Contact from "./components/Contact";
 import RestaurantMenu from "./components/RestaurantsMenu";
-import Profile from "./components/Profile";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Shimmer from "./components/Loader";
 import useOnline from "./utils/useOnline";
